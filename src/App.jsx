481755b990050226da36f9d9a3d8befa6925c61f
SihamELBOUBAKRI <155733015+SihamELBOUBAKRI@users.noreply.github.com>
2024-11-21 03:17:42 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+// api: http://api.themoviedb.org/3/search/tv?query=breaking&api_key=fef55a6754f2f6d00a0038388915039c
+function App() {
+  const [movieName,setmovieName]=useState("");
+  const [search,setsearch]=useState([]);
+
+
+  function movieFunction(e){
+    setmovieName(e.target.value);
+  }
+
+  const searchbutton = async (e)=>{
+    e.preventDefault();
+    const data = await axios.get(`https://api.themoviedb.org/3/search/tv?query=${movieName}g&api_key=fef55a6754f2f6d00a0038388915039c`)
+    setsearch(data.data.results);
+
+  }
+  return (
+    <div className="App">
+      <form action="">
+        <input type="text" placeholder='enter the movie name' value={movieName} onChange={movieFunction}/>
+        <button onClick={searchbutton}>search</button>
+      </form>
+
+      <div >
+        {
+          search.map((movie)=>{
+            return(
+              <div key={movie.id}>
+                console.log({movie.name})
+                <h1>{movie.name}</h1>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  );
+}
+
+export default App;
